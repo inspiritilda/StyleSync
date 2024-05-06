1bd6e546c5a8eb0b0444ac72040fedb57a8ebdc2
Charley Liao <c24liao@gmail.com>
2024-05-06 12:42:55 -0700

    Add db

--- code/models.py
+++ code/models.py
@@ -31,7 +31,51 @@ class User(db.Model):
 # Outfit Table
 class Outfit(db.Model):
     id = mapped_column(Integer, primary_key=True)
-    user_id = mapped_column(Integer, ForeignKey('user.id'))
+    user_id = mapped_column(Integer, ForeignKey('User.id'), nullable=False)
     date = mapped_column(DATETIME, nullable=False)
     rating = mapped_column(Integer, nullable=True)
+    layer1 = mapped_column(Integer, ForeignKey('Layer1.id'), nullable=True)
+    layer2 = mapped_column(Integer, ForeignKey('Layer2.id'), nullable=True)
+    layer3 = mapped_column(Integer, ForeignKey('Layer3.id'), nullable=True)
+
+# After creating the below categories for db, we need to add the foreign keys into Outfit Table
+
+class weather(db.Model):
+    id = mapped_column(Integer, primary_key=True)
+    name = mapped_column(String(500), nullable=False)
+
+class colour(db.Model):
+    id = mapped_column(Integer, primary_key=True)
+    name = mapped_column(String(500), nullable=False)
+
+class feeling(db.Model):
+    id = mapped_column(Integer, primary_key=True)
+    name = mapped_column(String(500), nullable=False)
+
+class style(db.Model):
+    id = mapped_column(Integer, primary_key=True)
+    name = mapped_column(String(500), nullable=False)
+
+# Layer 1 Table (Shirt)
+class Layer1(db.Model):
+    id = mapped_column(Integer, primary_key=True)
+    name = mapped_column(String(500), nullable=False)
+    style_id = mapped_column(Integer, ForeignKey('style.id'))
+    feeling_id = mapped_column(Integer, ForeignKey('feeling.id'))
+    colour_id = mapped_column(Integer, ForeignKey('colour.id'))
+    weather_id = mapped_column(Integer, ForeignKey('weather.id'))
+
+# Layer 2 Table (Sweater)
+
+# Layer 3 Table (Jacket)
+
+# LegWear
+
+# FootWear (Socks)
+
+# ShoeWear
+
+# Accessory 1
+
+# Accessory 2
 
