3b3fef9f6016e69cf69e726b2b2545a764cde917
teesar <mishamakaroff@gmail.com>
2024-05-08 10:49:03 -0700

    html navigation setup

--- code/routes/html.py
+++ code/routes/html.py
@@ -10,3 +10,15 @@ html_routes_bp = Blueprint("html", __name__)
 @html_routes_bp.route("/home")
 def home():
     return render_template("/html/index.html")
+
+@html_routes_bp.route("/homepage")
+def homepage():
+    return render_template("/html/home.html")
+
+@html_routes_bp.route("/newoutfit")
+def newoutfit():
+    return render_template("/html/newoutfit.html")
+
+@html_routes_bp.route("/wardrobe")
+def wardrobe():
+    return render_template("/html/wardrobe.html")
