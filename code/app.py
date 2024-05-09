b1ca0db83a8e5805778de7e5907fd2a0d6f3c462
Charley Liao <c24liao@gmail.com>
2024-05-09 14:48:10 -0700

    add project proposal final

--- code/app.py
+++ code/app.py
@@ -2,19 +2,28 @@ from flask import Flask, url_for, session, redirect
 from db import db
 from pathlib import Path
 from routes import auth_routes_bp, html_routes_bp
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # Initialize the Flask app
-app = Flask(__name__)
-app.secret_key = "supersecret"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
-app.instance_path = Path("./data").resolve()
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"]= "supersecret"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+    app.instance_path = Path("./data").resolve()
 
-# Initialize the database
-db.init_app(app)
-app.register_blueprint(auth_routes_bp, url_prefix="/")
-app.register_blueprint(html_routes_bp, url_prefix="/views")
+    db.init_app(app)
+    app.register_blueprint(auth_routes_bp, url_prefix="/")
+    app.register_blueprint(html_routes_bp, url_prefix="/views")
+    return app
+
+
+# # Initialize the database
+# db.init_app(app)
+# app.register_blueprint(auth_routes_bp, url_prefix="/")
+# app.register_blueprint(html_routes_bp, url_prefix="/views")
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True, port=8888)
