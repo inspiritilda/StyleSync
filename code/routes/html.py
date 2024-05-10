06f5390ffee395e4dee15d47399d8ba91d9b546c
teesar <mishamakaroff@gmail.com>
2024-05-09 22:42:35 -0700

    temp change asset folder inside static to pics for troubleshooting

--- code/routes/html.py
+++ code/routes/html.py
@@ -20,10 +20,10 @@ def newoutfit():
 @html_routes_bp.route("/wardrobe")
 def wardrobe():
     #this is used to populate the html wardrobe grid with pictures in assets folder - temporary
-    folder_path = 'static/Assets'
+    folder_path = 'static/pics'
     image_files = [filename for filename in os.listdir(folder_path)
                    if filename.endswith(('.jpg', '.jpeg', '.png', '.gif'))]
-    image_urls = [url_for('static', filename=f'Assets/{filename}') for filename in image_files]
+    image_urls = [url_for('static', filename=f'pics/{filename}') for filename in image_files]
     print(image_urls)
 
     return render_template("/html/wardrobe.html", image_urls=image_urls)
